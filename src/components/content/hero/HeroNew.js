ebb36452478b7b5b201bb2f215dd895d4d8637c5
michaelpsheehan <sheehanpmichael@gmail.com>
2020-07-20 19:52:06 +0100

    hero scroll scrub 0.1

--- src/components/content/hero/HeroNew.js
+++ src/components/content/hero/HeroNew.js
@@ -115,7 +115,7 @@ class HeroNew extends Component {
     const tlScroll = gsap.timeline({
       scrollTrigger: {
         start: 'top top',
-        scrub: 0.3,
+        scrub: 0.1,
       },
     })
     tlScroll.to([...this.allWords[0], ...this.allWords[1]], {
