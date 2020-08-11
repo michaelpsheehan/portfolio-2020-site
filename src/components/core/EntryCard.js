cf2ba7ae04f686b59c0a33521789efc51290a334
Michael Sheehan <sheehanpmichael@gmail.com>
2020-08-11 14:48:30 +0100

    entry card transition bug fix

--- src/components/core/EntryCard.js
+++ src/components/core/EntryCard.js
@@ -1,61 +1,30 @@
 import React, { useEffect } from 'react'
-import { Link } from 'gatsby'
 import AniLink from 'gatsby-plugin-transition-link/AniLink'
-import TransitionLink from 'gatsby-plugin-transition-link'
 import Button from './Button'
 import Image from './Image'
 
-import PageTransition from './PageTransition'
 
-import FadeLink from './transitions/FadeLink'
 
-const EntryCard = ({ item, siteUrl, itemSection = '', classes = '' }) => {
+const EntryCard = ({ item, itemSection = '', classes = '' }) => {
   const {
     slug,
     title,
     thumbnailDescription,
     imageCaption,
-    introBody,
-    uri,
-    url,
   } = item
   const entryCardImage = item.heroImage[0]
     ? item.heroImage[0].imageOptimizeSmallImage
     : null
 
-  const interestingExitAnimation = (item1, item2) => (
-    // console.log('intersting animation --', item1, item2)
-    <PageTransition />
-  )
 
   return (
     <div className={`c-entry-card ${classes}`}>
       <AniLink
-        // duration="0.3"
         className="bg-brand-blue"
-        // cover
         paintDrip
-        // direction="left"
-        exit="cover"
-        // hex="#5891C4"
         hex="#1A68AF"
         to={`/${itemSection}/${slug}`}
       >
-        {/* <TransitionLink
-        to={`/${itemSection}/${slug}`}
-        exit={{
-          trigger: ({ exit, node }) => {
-            // console.log('exit transitio started', exit, node)
-            interestingExitAnimation(exit, node)
-          },
-          length: 1,
-        }}
-        entry={{
-          delay: 2.6,
-        }}
-      > */}
-
-        {/* <FadeLink to={`/${itemSection}/${slug}`}> */}
         {entryCardImage && (
           <Image
             image={entryCardImage}
@@ -77,10 +46,7 @@ const EntryCard = ({ item, siteUrl, itemSection = '', classes = '' }) => {
             </div>
           </>
         )}
-        {/* </Link> */}
-        {/* </TransitionLink> */}
       </AniLink>
-      {/* </FadeLink> */}
     </div>
   )
 }
