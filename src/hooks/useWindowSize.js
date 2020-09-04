9b260f0d67a2970e2d1646ebbdecfc9aca30fa81
Michael Sheehan <sheehanpmichael@gmail.com>
2020-09-04 15:33:05 +0100

    more debug

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -14,7 +14,7 @@ export default function useWindowSize() {
   }
 
   const [windowSize, setWindowSize] = useState(getSize)
-
+// let prevHeight;
 
   useEffect(() => {
     // listen for window resizes and run the function again to update the values
@@ -30,7 +30,7 @@ export default function useWindowSize() {
     // update the value for the vh css custom property on resize. 
     // This ensures mobile devices have a true 100vh on mobile without the address bar making 100vh incorrect. falls back to standard 100vh if the browser does not support custom properties
     document.documentElement.style.setProperty('--vh', `${vh}px`)
-  },[windowSize])
+  },[windowSize.height])
   
   return isBrowser ? windowSize : null
 }
