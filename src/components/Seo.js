9fe4e2d22595a07a965f813fc17b10b446489376
Michael Sheehan <sheehanpmichael@gmail.com>
2020-10-05 12:37:26 +0100

    meta size test

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -84,10 +84,10 @@ const SEO = ({ title, description, socialMeta }) => {
         />
       )}
 
-      <meta content="en_GB" property="og:locale" />
+      {/* <meta content="en_GB" property="og:locale" />
       <meta content="website" property="og:type" />
       <meta content="1200" property="og:image:width" />
-      <meta content="630" property="og:image:height" />
+      <meta content="630" property="og:image:height" /> */}
     </Helmet>
   )
 }
